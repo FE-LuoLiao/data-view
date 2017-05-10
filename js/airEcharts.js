51a3139fb6764a7ef6c7247da9abf65ed6059087
YaWen21 <2656183413@qq.com>
2017-05-10 17:43:13 +0800

    LowerMiddleLeftchange

--- js/airEcharts.js
+++ js/airEcharts.js
@@ -1,6 +1,6 @@
 var myChart = echarts.init(document.getElementsByClassName('first-content-left')[0]);
-     myChartTopMiddle = echarts.init(document.getElementsByClassName('top-middle-content-bottom')[0]);
-var airAllData;  // 保存所有空气质量数据
+
+var  airAllData;  // 保存所有空气质量数据
 //地图的option
 var optionTop = {
     backgroundColor: '#404a59',
@@ -69,72 +69,6 @@ var optionTop = {
     }
 };
 
-var optionTopMiddle = {
-    title: {
-        text: '浏览器占比变化',
-        subtext: '纯属虚构',
-        x:'right',
-        y:'bottom'
-    },
-    tooltip: {
-        trigger: 'item',
-        backgroundColor : 'rgba(0,0,250,0.2)'
-    },
-    legend: {
-        data: (function (){
-            var list = [];
-            for (var i = 1; i <=28; i++) {
-                list.push(i + 2000 + '');
-            }
-            return list;
-        })()
-    },
-    visualMap: {
-        color: ['red', 'yellow']
-    },
-    radar: {
-        indicator : [
-            { text: 'IE8-', max: 400},
-            { text: 'IE9+', max: 400},
-            { text: 'Safari', max: 400},
-            { text: 'Firefox', max: 400},
-            { text: 'Chrome', max: 400}
-        ]
-    },
-    series : (function (){
-        var series = [];
-        for (var i = 1; i <= 28; i++) {
-            series.push({
-                name:'浏览器（数据纯属虚构）',
-                type: 'radar',
-                symbol: 'none',
-                itemStyle: {
-                    normal: {
-                        lineStyle: {
-                            width:1
-                        }
-                    },
-                    emphasis : {
-                        areaStyle: {color:'rgba(0,250,0,0.3)'}
-                    }
-                },
-                data:[
-                    {
-                        value:[
-                            (40 - i) * 10,
-                            (38 - i) * 4 + 60,
-                            i * 5 + 10,
-                            i * 9,
-                            i * i /2
-                        ],
-                        name: i + 2000 + ''
-                    }
-                ]
-            });
-        }
-        return series;
-    })()
-};
 //第一部分ajax请求
 function getDetailData(url, callback) {
     //  TODO
@@ -158,7 +92,8 @@ getDetailData('./data/china.json', function (data) {
 
     //ajax请求数据
     getDetailData('./data/air-quality.json', function (data) {
-        airAllData = JSON.parse(data.response);
+        airAllData = JSON.parse(data.response);//获取所有数据
+       // console.log(airAllData)
         var array = [];
         var airData = JSON.parse(data.response);
         for (var i = 0; i < airData.length; i++) {
--- js/airQuality.js
+++ js/airQuality.js
@@ -6,16 +6,16 @@ var catTitleList = document.getElementsByClassName('cat-title-list')[0],
     list = catTitleList.getElementsByTagName('a');
 
 
-catTitleList.addEventListener('click',catTitleHandler);
-catAnotherTitleList.addEventListener('click',catAnotherTitleHandler);
+catTitleList.addEventListener('click', catTitleHandler);
+catAnotherTitleList.addEventListener('click', catAnotherTitleHandler);
 // displayContent.addEventListener('click',displayHandler);
 
 
 //改变样式
-function catTitleHandler(event){
+function catTitleHandler(event) {
     var lastHeight = document.getElementsByClassName('cat-active-list')[0];
 
-    if(lastHeight !== undefined){
+    if (lastHeight !== undefined) {
         lastHeight.className = '';
     }
     event.target.className = 'cat-active-list';
@@ -23,18 +23,18 @@ function catTitleHandler(event){
     clickDetailChanges();
 }
 //bottom block changes style
-function catAnotherTitleHandler(event){
+function catAnotherTitleHandler(event) {
     var bottomHeight = document.getElementsByClassName('last-content-head')[0];
-    if(bottomHeight !== undefined){
+    if (bottomHeight !== undefined) {
         bottomHeight.className = '';
     }
     event.target.className = 'last-content-head';
 }
 //获取存储数据
-function clickDetailChanges(){
+function clickDetailChanges() {
     var obtainP = document.getElementsByClassName('air-details-content')[0];
     var obtainH = document.getElementsByClassName('air-title-content')[0];
-    var index = event.target.dataset['index']-0;
+    var index = event.target.dataset['index'] - 0;
     // debugger;
     obtainH.innerHTML = obtainContent.title[index];
     obtainP.innerHTML = obtainContent.content[index];
@@ -44,10 +44,29 @@ function clickDetailChanges(){
 
 
 }
-
+// //ajax获取数据P标签
+function getDetailData(url, callback) {
+    //  TODO
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            callback(xmlhttp);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+//调用函数
+getDetailData('./data/air-detail.json', function (data) {
+    obtainContent = JSON.parse(data.response);
+    var obtainP = document.getElementsByClassName('air-details-content')[0];
+    var obtainH = document.getElementsByClassName('air-title-content')[0];
+    obtainH.innerHTML = obtainContent.title[0];
+    obtainP.innerHTML = obtainContent.content[0];
+});
 //得到地图的数据
 function getChartsData(index) {
-    var cats = ['aqi', 'pm2_5', 'pm10','co','no2','o3','so2'];
+    var cats = ['aqi', 'pm2_5', 'pm10', 'co', 'no2', 'o3', 'so2'];
     var array = [];
     for (var i = 0; i < airAllData.length; i++) {
         array.push({
@@ -60,31 +79,167 @@ function getChartsData(index) {
     myChart.setOption(optionTop);
 }
 
-//topMiddle的数据
-function getChartsTopMiddleData(){
+// //topMiddle的数据
+function getChartsTopMiddleData() {
 
+    var myChartTopMiddle = echarts.init(document.getElementsByClassName('top-middle-content-bottom')[0]);
+    var optionTopMiddle = {
+
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: [{
+            left: '70%',
+            right: '20%',
+            bottom: '3%',
+            containLabel: true
+        }, {
+            left: '50%',
+            right: '40%',
+            bottom: '3%',
+            containLabel: true
+        }],
+        xAxis: [
+            {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        radar: {
+            indicator: [
+                {text: 'IE8-', max: 400},
+                {text: 'IE9+', max: 400},
+                {text: 'Safari', max: 400},
+                {text: 'Firefox', max: 400},
+                {text: 'Chrome', max: 400}
+            ]
+        },
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '60%',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+]
+};
+myChartTopMiddle.setOption(optionTopMiddle);
 }
-
-
-//ajax获取数据P标签
-function getDetailData(url,callback) {
-    //  TODO
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            callback(xmlhttp);
-        }
+getChartsTopMiddleData();
+//lowerMiddle的数据
+function getChartsLowerMiddleLeft() {
+    var myChartsLowerMiddle = echarts.init(document.getElementsByClassName('lower-middle-content-left')[0]);
+    var data = [
+        [[28604,77,17096869,'Australia',1990],[31163,77.4,27662440,'Canada',1990],[1516,68,1154605773,'China',1990],[13670,74.7,10582082,'Cuba',1990],[28599,75,4986705,'Finland',1990],[29476,77.1,56943299,'France',1990],[31476,75.4,78958237,'Germany',1990],[28666,78.1,254830,'Iceland',1990],[1777,57.7,870601776,'India',1990],[29550,79.1,122249285,'Japan',1990],[2076,67.9,20194354,'North Korea',1990],[12087,72,42972254,'South Korea',1990],[24021,75.4,3397534,'New Zealand',1990],[43296,76.8,4240375,'Norway',1990],[10088,70.8,38195258,'Poland',1990],[19349,69.6,147568552,'Russia',1990],[10670,67.3,53994605,'Turkey',1990],[26424,75.7,57110117,'United Kingdom',1990],[37062,75.4,252847810,'United States',1990]],
+        [[44056,81.8,23968973,'Australia',2015],[43294,81.7,35939927,'Canada',2015],[13334,76.9,1376048943,'China',2015],[21291,78.5,11389562,'Cuba',2015],[38923,80.8,5503457,'Finland',2015],[37599,81.9,64395345,'France',2015],[44053,81.1,80688545,'Germany',2015],[42182,82.8,329425,'Iceland',2015],[5903,66.8,1311050527,'India',2015],[36162,83.5,126573481,'Japan',2015],[1390,71.4,25155317,'North Korea',2015],[34644,80.7,50293439,'South Korea',2015],[34186,80.6,4528526,'New Zealand',2015],[64304,81.6,5210967,'Norway',2015],[24787,77.3,38611794,'Poland',2015],[23038,73.13,143456918,'Russia',2015],[19360,76.5,78665830,'Turkey',2015],[38225,81.4,64715810,'United Kingdom',2015],[53354,79.1,321773631,'United States',2015]]];
+    var optionLowerMiddle = {
+        backgroundColor: new echarts.graphic.RadialGradient(0.3, 0.3, 0.8, [{
+            offset: 0,
+            color: '#f7f8fa'
+        }, {
+            offset: 1,
+            color: '#cdd0d5'
+        }]),
+        title: {
+            text: '1990 与 2015 年各国家人均寿命与 GDP'
+        },
+        legend: {
+            right: 10,
+            data: ['1990', '2015']
+        },
+        xAxis: {
+            splitLine: {
+                lineStyle: {
+                    type: 'dashed'
+                }
+            }
+        },
+        yAxis: {
+            splitLine: {
+                lineStyle: {
+                    type: 'dashed'
+                }
+            },
+            scale: true
+        },
+        series: [{
+            name: '1990',
+           // data: data[0],
+            type: 'scatter',
+            symbolSize: function (data) {
+                return Math.sqrt(data[2]) / 5e2;
+            },
+            label: {
+                emphasis: {
+                    show: true,
+                    formatter: function (param) {
+                        return param.data[3];
+                    },
+                    position: 'top'
+                }
+            },
+            itemStyle: {
+                normal: {
+                    shadowBlur: 10,
+                    shadowColor: 'rgba(120, 36, 50, 0.5)',
+                    shadowOffsetY: 5,
+                    color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
+                        offset: 0,
+                        color: 'rgb(251, 118, 123)'
+                    }, {
+                        offset: 1,
+                        color: 'rgb(204, 46, 72)'
+                    }])
+                }
+            }
+        }, {
+            name: '2015',
+           // data: data[1],
+            type: 'scatter',
+            symbolSize: function (data) {
+                return Math.sqrt(data[2]) / 5e2;
+            },
+            label: {
+                emphasis: {
+                    show: true,
+                    formatter: function (param) {
+                        return param.data[3];
+                    },
+                    position: 'top'
+                }
+            },
+            itemStyle: {
+                normal: {
+                    shadowBlur: 10,
+                    shadowColor: 'rgba(25, 100, 150, 0.5)',
+                    shadowOffsetY: 5,
+                    color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
+                        offset: 0,
+                        color: 'rgb(129, 227, 238)'
+                    }, {
+                        offset: 1,
+                        color: 'rgb(25, 183, 207)'
+                    }])
+                }
+            }
+        }]
     };
-    xmlhttp.open("GET",url,true);
-    xmlhttp.send();
+    myChartsLowerMiddle.setOption(optionLowerMiddle);
 }
-//调用函数
-getDetailData('./data/air-detail.json',function(data){
-    obtainContent = JSON.parse(data.response);
-    var obtainP = document.getElementsByClassName('air-details-content')[0];
-    var obtainH = document.getElementsByClassName('air-title-content')[0];
-    obtainH.innerHTML = obtainContent.title[0];
-    obtainP.innerHTML = obtainContent.content[0];
-});
+getChartsLowerMiddleLeft();
+
+
 
 
